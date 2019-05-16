e634e2f195f315fcf76ca8280adaf3b82b03e931
DevTesmari <devtest1mari@gmail.com>
2019-05-16 16:21:37 +0900

    wip

--- scripts/git_build_and_push_dist.sh
+++ scripts/git_build_and_push_dist.sh
@@ -11,7 +11,7 @@ fi
 git checkout master
 mkdir -p ~/.git && git config user.email "devdoomari@gmail.com" && git config user.name "devdoomari.circleci"
 git merge -X theirs --no-edit $GIT_BRANCH
-npm run clean && npm run build
+
 git add -f lib docs
 git commit --allow-empty -m $'generated from:'"$GIT_COMMIT_HASH"$'\ntriggered by:'"$CI_USERNAME"$'\n[ci skip]'
 git tag -a $'dist_'"$GIT_BRANCH"'_'"$CI_BUILD_NUM" -m "."
